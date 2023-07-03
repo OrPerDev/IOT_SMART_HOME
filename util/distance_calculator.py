5c117d5f57ab8563c228137aa73427c287154c32
Shahar Tal <chapo12345@gmail.com>
2023-07-03 20:13:20 +0300

    added gps distance calculator

--- util/distance_calculator.py
+++ util/distance_calculator.py
@@ -4,11 +4,18 @@ from math import sin, cos, sqrt, atan2, radians
 # Approximate radius of earth in km
 R = 6373.0
 
+
 def calc_distance_km(c1: Tuple[float, float], c2: Tuple[float, float]) -> float:
     """
     Calculate distance between two gps coordinates in meters.
     Coordinates should be in the form of (latitude, longitude).
     """
+    if not isinstance(c1, tuple) or not isinstance(c2, tuple):
+        raise TypeError("Coordinates should be tuples")
+
+    if len(c1) != 2 or len(c2) != 2:
+        raise TypeError("Coordinates should be tuples of length 2")
+
     lat1 = radians(c1[0])
     lon1 = radians(c1[1])
     lat2 = radians(c2[0])
@@ -17,14 +24,15 @@ def calc_distance_km(c1: Tuple[float, float], c2: Tuple[float, float]) -> float:
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
     distance_km = R * c
 
     return distance_km
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     holon_mall = (32.01278924606207, 34.77908810682126)
     hit = (32.01487634797979, 34.77458326803195)
 
---
+++ util/test_distance_calculator.py
@@ -0,0 +1,48 @@
+import pytest
+from distance_calculator import calc_distance_km
+
+
+def test_calc_distance_km_should_return_correct_distance():
+    holon_mall = (32.01278924606207, 34.77908810682126)
+    hit = (32.01487634797979, 34.77458326803195)
+
+    distance = calc_distance_km(holon_mall, hit)
+
+    assert distance == pytest.approx(0.48, 0.01)
+
+
+def test_calc_distance_km_should_return_zero_when_same_coordinates():
+    c1 = (32.01278924606207, 34.77908810682126)
+    c2 = (32.01278924606207, 34.77908810682126)
+
+    distance = calc_distance_km(c1, c2)
+
+    assert distance == pytest.approx(0.0, 0.01)
+
+
+@pytest.mark.parametrize(
+    "c1, c2",
+    [
+        (32.01278924606207, (32.01278924606207, 34.77908810682126)),
+        ((32.01278924606207, 34.77908810682126), 32.01278924606207),
+    ],
+)
+def test_calc_distance_km_should_fail_when_input_is_not_tuple(c1, c2):
+    with pytest.raises(TypeError):
+        distance = calc_distance_km(c1, c2)
+
+
+def test_calc_distance_km_should_fail_when_input_is_not_tuple_of_floats():
+    c1 = (32.01278924606207, 34.77908810682126)
+    c2 = (32.01278924606207, "34.77908810682126")
+
+    with pytest.raises(TypeError):
+        distance = calc_distance_km(c1, c2)
+
+
+def test_calc_distance_km_should_fail_when_input_is_not_tuple_of_length_2():
+    c1 = (32.01278924606207, 34.77908810682126)
+    c2 = (32.01278924606207, 34.77908810682126, 34.77908810682126)
+
+    with pytest.raises(TypeError):
+        distance = calc_distance_km(c1, c2)
