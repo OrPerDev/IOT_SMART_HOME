a1d514f4d876d1cc8aacf74da01a682c7b12db96
Shahar Tal <stalmail10@gmail.com>
2023-08-04 18:22:03 +0300

    removed redundant old test files
    Signed-off-by: Shahar Tal <stalmail10@gmail.com>

--- code/emulators/collar_location_client/src/main.py
+++ code/emulators/collar_location_client/src/main.py
@@ -1,11 +1,6 @@
 from gps_sensor import new_gps_sensor
 from network_interface import NetworkInterface
-import os
 from common.environment import COLLAR_ID, PET_GPS_SENSOR_CONFIG
-
-
-def absolute_path(relative_path: str) -> str:
-    return os.path.join(os.path.dirname(__file__), relative_path)
 
 
 if __name__ == "__main__":
